a04df9ec01dd440a9b45193810e5bd4f40fc082a
doriancodz <122804204+doriancodz@users.noreply.github.com>
2023-04-02 15:28:25 +0430

    added the search engine

--- src/script.js
+++ src/script.js
@@ -40,8 +40,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "f4b0da9b390c0fa431a1tef05b8o545f";
-let city = "New York";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "f4b0da9b390c0fa431a1tef05b8o545f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
